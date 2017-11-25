e48dc9d9bf2a87554a5d32388df94a587ead1170
Thomas Graf <tngraf@gmx.de>
2017-11-25 18:25:47 +0100

    Nuget package information updated and packages published

--- BuildPackages.ps1
+++ BuildPackages.ps1
@@ -3,47 +3,62 @@
 # =======================================
 
 cd Tethys.Logging
-nuget pack Tethys.Logging.csproj
+nuget pack Tethys.Logging.nuspec
 Move-Item Tethys.Logging.*.nupkg ..\export\packages
 cd ..
 
 cd Tethys.Logging.Controls
-nuget pack Tethys.Logging.Controls.csproj
+nuget pack Tethys.Logging.Controls.nuspec
 move Tethys.Logging.*.nupkg ..\export\packages
 cd ..
 
 cd Tethys.Logging.Controls.Wpf
-nuget pack Tethys.Logging.Controls.Wpf.csproj
+nuget pack Tethys.Logging.Controls.Wpf.nuspec
 move Tethys.Logging.*.nupkg ..\export\packages
 cd ..
 
 cd Tethys.Logging.Common.Logging
-nuget pack Tethys.Logging.Common.Logging.csproj
+nuget pack Tethys.Logging.Common.Logging.nuspec
 move Tethys.Logging.*.nupkg ..\export\packages
 cd ..
 
 cd Tethys.Logging.Common.Logging231
-nuget pack Tethys.Logging.Common.Logging231.csproj
+nuget pack Tethys.Logging.Common.Logging231.nuspec
+move Tethys.Logging.*.nupkg ..\export\packages
+cd ..
+
+cd Tethys.Logging.Common.Logging341
+nuget pack Tethys.Logging.Common.Logging341.nuspec
 move Tethys.Logging.*.nupkg ..\export\packages
 cd ..
 
 cd Tethys.Logging.Log4net
-nuget pack Tethys.Logging.Log4net.csproj
+nuget pack Tethys.Logging.Log4net.nuspec
+move Tethys.Logging.*.nupkg ..\export\packages
+cd ..
+
+cd Tethys.Logging.Log4net208
+nuget pack Tethys.Logging.Log4net208.nuspec
 move Tethys.Logging.*.nupkg ..\export\packages
 cd ..
 
 cd Tethys.Logging.NLog
-nuget pack Tethys.Logging.NLog.csproj
+nuget pack Tethys.Logging.NLog.nuspec
 move Tethys.Logging.*.nupkg ..\export\packages
 cd ..
 
 cd Tethys.Logging.NLog310
-nuget pack Tethys.Logging.NLog310.csproj
+nuget pack Tethys.Logging.NLog310.nuspec
+move Tethys.Logging.*.nupkg ..\export\packages
+cd ..
+
+cd Tethys.Logging.NLog4412
+nuget pack Tethys.Logging.NLog4412.nuspec
 move Tethys.Logging.*.nupkg ..\export\packages
 cd ..
 
 cd Tethys.Logging.Win
-nuget pack Tethys.Logging.Win.csproj
+nuget pack Tethys.Logging.Win.nuspec
 move Tethys.Logging.*.nupkg ..\export\packages
 cd ..
 
