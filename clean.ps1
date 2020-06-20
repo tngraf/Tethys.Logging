836a9edc027381afc5e3694940a595e33f046a19
Thomas Graf <tngraf@gmx.de>
2020-06-20 18:03:57 +0200

    manual build scripts updated

---
+++ clean.ps1
@@ -0,0 +1,43 @@
+# -------------
+# Clean project
+# -------------
+
+dotnet clean
+Remove-Item "Tethys.Logging\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.Common.Logging\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.Common.Logging\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.Common.Logging231\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.Common.Logging231\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.Common.Logging341\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.Common.Logging341\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.Console\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.Console\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.Controls\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.Controls\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.Controls.Wpf\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.Controls.Wpf\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.Log4Net\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.Log4Net\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.Log4Net208\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.Log4Net208\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.NLog\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.NLog\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.NLog310\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.NLog310\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.NLog4412\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.NLog4412\obj" -Recurse -ErrorAction SilentlyContinue
+
+Remove-Item "Tethys.Logging.Win\bin" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "Tethys.Logging.Win\obj" -Recurse -ErrorAction SilentlyContinue
